f1482b7e029acdc09eb502d95d6e92e783771f48
Danilo <didox_59@yahoo.com.br>
2019-05-16 23:04:24 -0300

    projeto aula zero a api

--- app/controllers/estados_controller.rb
+++ app/controllers/estados_controller.rb
@@ -1,6 +1,5 @@
 class EstadosController < ApplicationController
   def index
-
     estados = [
       {nome:"São Paulo",uf:"SP"},
       {nome:"Rio de Jneiro",uf:"RJ"},
@@ -8,8 +7,29 @@ class EstadosController < ApplicationController
       {nome:"Acre",uf:"AC"}
     ]
 
+    if params["uf"].present?
+      estados = estados.select{|e| e[:uf].downcase == params["uf"].downcase }
+    end
+
     render json: estados, status: 200
+  end
+
+  def lista_cidades
+    cidades = [
+      {nome:"São Paulo",uf:"SP"},
+      {nome:"São Bernardo do campo",uf:"SP"},
+      {nome:"Mauá",uf:"SP"},
+      {nome:"Campinas",uf:"SP"},
+      {nome:"Niteroi",uf:"RJ"},
+      {nome:"Cabo frio",uf:"RJ"},
+      {nome:"Copacabana",uf:"RJ"},
+    ]
+
+    if params["uf"].present?
+      cidades = cidades.select{|e| e[:uf].downcase == params["uf"].downcase }
+    end
     
+    render json: cidades, status: 200
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :carros
 
   get '/estados', to: 'estados#index'
+  get '/cidades', to: 'estados#lista_cidades'
 end
